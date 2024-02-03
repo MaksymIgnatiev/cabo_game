f47a2cd463f606b83545e99b69a2255082374c30
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-02-03 20:54:55 +0100

    first commit from next js app

---
+++ src/app/home/ok.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function ok() {
+	return <div>ok</div>
+}
+// https://discord.com/channels/447699225078136832/573904861037592589/1203006910572273708
---
+++ src/app/home/page.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Home() {
+	return <h1 style={{ margin: "3em" }}>home</h1>
+}
