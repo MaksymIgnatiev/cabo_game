722032215a9f655d4f03e9e0247a3b72928305a4
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-03-03 18:38:32 +0100

    ops...

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -17,6 +17,7 @@ export default function MainGame() {
 			{/* 
 			let me check one thing
 			yes, it's definietly that thing i have checked
+			there was a problem, but we fixed that :)
 			*/}
 			<div class="game">
 				<div>
