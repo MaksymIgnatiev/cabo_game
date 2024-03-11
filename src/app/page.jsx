7be342db5cd702d13882263779b036e1c505f00f
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-03-11 20:54:48 +0100

    added some configuring files and prisma

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-
 import styles from "./page.module.css"
 import MainGame from "@/components/MainGame"
 export default function Home() {
@@ -9,3 +8,5 @@ export default function Home() {
 		</>
 	)
 }
+// 17 * 24
+// 3 * 24
