421f928d347cd1c6b6e0f9a306d4923338d078b5
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-03-15 17:59:26 +0100

    another some minor changes

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -1,9 +1,6 @@
 "use client"
 
-import { useState } from "react"
-
 export default function MainGame() {
-	const [as, setAs] = useState(Array.from({length: 10}, (_, i) => i + 1))
 	return (
 		<div className="wrapper">
 			<div className="top">
@@ -12,9 +9,6 @@ export default function MainGame() {
 				</div>
 			</div>
 			<div className="game">
-				<div>
-					{as.map(e => <a key={e}>{e}</a>)}
-				</div>
 				<h1>финансирование поступило, сайт в разработке</h1>
 			</div>
 		</div>
