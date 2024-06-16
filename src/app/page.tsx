cc6f53990c5af340cb8bbd1fb9f5aa014c94fc4d
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-06-16 22:47:36 +0200

    Commit?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@
 
 import "./scss/main.scss"
 
-import { useEffect, useState } from "react"
+import { GameUser, HomePageComponentParams, User } from "../../types"
+import {
+	LOCAL_STORAGE_NICKNAME_KEY,
+	LOCAL_STORAGE_ROOM_ID_KEY,
+	LOCAL_STORAGE_USER_ID_KEY,
+	LOCAL_STORAGE_USER_SETTINGS_KEY,
+} from "./data/keys"
 import {
 	createDefaultUserSettings,
 	createUser,
@@ -11,21 +17,15 @@ import {
 	loadToGlobalView,
 	sendMessageToWebSocket,
 } from "../../functions"
-import { GameUser, HomePageComponentParams, User } from "../../types"
-import {
-	LOCAL_STORAGE_NICKNAME_KEY,
-	LOCAL_STORAGE_ROOM_ID_KEY,
-	LOCAL_STORAGE_USER_ID_KEY,
-	LOCAL_STORAGE_USER_SETTINGS_KEY,
-} from "./data/keys"
+import { useEffect, useState } from "react"
 
-import { counter } from "../../database"
 import CollectData from "./components/CollectData"
 import Loading from "./components/Loading"
 import MainGame from "./components/MainGame"
 import Settings from "./components/Settings"
 import Start from "./components/Start"
 import Transition from "./components/Transition"
+import { counter } from "../../database"
 
 export default function Home({ URLRoomId }: HomePageComponentParams) {
 	const [user, setUser] = useState<User | GameUser>(
@@ -61,6 +61,7 @@ export default function Home({ URLRoomId }: HomePageComponentParams) {
 			return () => {
 				// ws.close()
 				// do not remove this comment!!!
+				// and this also
 			}
 		},
 		toggleTransition = () => {
