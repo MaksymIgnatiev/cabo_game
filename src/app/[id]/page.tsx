85edb1551664933ef1daa31ddd58f5fe804b8b00
Maksym Ignatiev <maksym.ignatiev2006@gmail.com>
2024-08-15 21:24:32 +0200

    H

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,7 +5,7 @@ import { useParams } from "next/navigation"
 import { useState } from "react"
 import { checkRoom } from "../../../functions"
 import InvalidId from "./error/InvalidId"
-import NoSuchGame from "./error/noSuchGame"
+import NoSuchGame from "./error/NoSuchGame"
 
 export default function ID() {
 	var [invalidId, setInvalidId] = useState(false),
