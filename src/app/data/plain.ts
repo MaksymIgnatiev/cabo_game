197bf181647cf503ee92b64a4f2e3302c3c94f1b
MaksymIgnatiev <maksym.ignatiev2006@gmail.com>
2025-01-12 21:27:34 +0100

    Changed lisence to GPL3

--- src/app/data/plain.ts
+++ src/app/data/plain.ts
@@ -1,7 +1,7 @@
-import { colorText } from "../../../functions"
+import { colorText } from "../../../functions";
 
-export var HEX_CHARS = "０１２３４５６７８９ａｂｃｄｅｆＡＢＣＤＥＦ",
-	INFO_SECTION = colorText("[INFO]", "#ac0"),
-	WARN_SECTION = colorText("[WARN]", "#cc0"),
-	ERROR_SECTION = colorText("[ERROR]", "#c00"),
-	LOG_SECTION = colorText("[LOG]", "#0cc")
+export var HEX_CHARS = "０１２３４５６７８９ａｂｃｄｅｆＡＢＣＤＥＦ" as const,
+  INFO_SECTION = colorText("[INFO]", "#09c"),
+  WARN_SECTION = colorText("[WARN]", "#cc0"),
+  ERROR_SECTION = colorText("[ERROR]", "#c00"),
+  LOG_SECTION = colorText("[LOG]", "#ccc");
